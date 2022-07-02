eebd97aed5723944bb25f16677e6de855954f645
e5k4p3 <e5k4p3@gmail.com>
2022-07-02 08:57:33 +0300

    Коммит для ревью

---
+++ src/Subtask.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Subtask extends Task {
+    private int epicId;
+
+    public Subtask(String name, String description, TaskStatus status, int epicId) {
+        super(name, description, status);
+        this.epicId = epicId;
+    }
+
+    public int getEpicId() {
+        return epicId;
+    }
+
+    public void setEpicId(int epicId) {
+        this.epicId = epicId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Subtask subtask = (Subtask) o;
+        return epicId == subtask.epicId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), epicId);
+    }
+
+    @Override
+    public String toString() {
+        return "Subtask{" +
+                "epicId=" + epicId +
+                '}';
+    }
+}
